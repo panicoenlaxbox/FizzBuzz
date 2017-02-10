de9d5bca5626cf499a200322069601f3ccf4c909
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-02-10 16:51:01 +0100

    Fizzbuzz working in initial stage

--- FizzBuzz/Class1.cs
+++ FizzBuzz/Class1.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
 
 namespace FizzBuzz
 {
-    public class Class1
+    public class FizzBuzShould
     {
+        // return the same number
+        // return fizz
+        // return buzz
+        // return fizzbuzz
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        [InlineData(3, "fizz")]
+        [InlineData(4, "4")]
+        [InlineData(5, "buzz")]
+        [InlineData(6, "fizz")]
+        [InlineData(15, "fizzbuzz")]
+        [InlineData(99, "fizz")]
+        public void return_correct_data_given_a_sample(int input, string expected)
+        {
+            var fizzBuzz = new FizzBuzzParser();
+            fizzBuzz.Parse(input).Should().Be(expected);
+        }
+    }
+
+    public class FizzBuzzParser
+    {
+        public string Parse(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            if (i % 3 == 0)
+            {
+                return "fizz";
+            }
+            if (i % 5 == 0)
+            {
+                return "buzz";
+            }
+            return i.ToString();
+        }
     }
 }
